ba794732b6727fdb433ad1bd35c431df38dbdecd
Jonssoni <n2nejo00@students.oamk.fi>
2024-11-04 20:20:38 +0200

    Update MainScreen.js

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,16 +1,78 @@
-// MainScreen.js
-import React from 'react';
-import styles from "../styles/style";
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 const MainScreen = ({ navigation }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+        const data = await response.json();
+        const randomCategories = getRandomCategories(data.categories, 4);
+        setCategories(randomCategories);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const getRandomCategories = (categories, count) => {
+    return categories.sort(() => 0.5 - Math.random()).slice(0, count);
+  };
+
   return (
-    <View style={styles.containerMain}>
-      <Text style={styles.titleMain}>Main Screen</Text>
-      <Text>Welcome to the Main Screen!</Text>
-     
+    <View style={styles.container}>
+      <Text style={styles.title}>Main Screen</Text>
+      <Text>Random meal categories:</Text>
+
+      <View style={styles.buttonContainer}>
+        <FlatList
+          data={categories}
+          keyExtractor={(item) => item.idCategory}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate('MealScreen', { category: item.strCategory })}
+            >
+              <Text style={styles.buttonText}>{item.strCategory}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '80%',
+  },
+  button: {
+    backgroundColor: 'steelblue',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+});
 
 export default MainScreen;
