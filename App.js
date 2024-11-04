e97e56330ce4b851a76538ae9da172151f92d1d1
Jonssoni <n2nejo00@students.oamk.fi>
2024-11-04 18:37:48 +0200

    push

--- App.js
+++ App.js
@@ -3,43 +3,54 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Header from "./screens/Header";
-import Footer from "./screens/Footer";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 import LoginScreen from './screens/LoginScreen';
 import CocktailScreen from './screens/CocktailScreen';
 import CocktailDetailScreen from './screens/CocktailDetailScreen';
 import MealScreen from './screens/MealScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const CocktailStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="CocktailScreen" component={CocktailScreen} options={{ title: 'Cocktail Search' }} />
+    <Stack.Screen name="CocktailDetail" component={CocktailDetailScreen} options={{ title: 'Cocktail Details' }} />
+  </Stack.Navigator>
+);
+
+const MealStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="MealScreen" component={MealScreen} options={{ title: 'Meal Search' }} />
+    <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{ title: 'Meal Details' }} />
+  </Stack.Navigator>
+);
+
 const App = () => {
   return (
-  <NavigationContainer>
-    <Tab.Navigator
-      sceneContainerStyle={{ backgroundColor: 'transparent' }}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-          if (route.name === 'MealScreen') {
-            iconName ='food';
-          } else if (route.name === 'CocktailScreen') {
-            iconName = 'glass-cocktail';
-          }
-          return (
-            <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          );
-        },
-        tabBarActiveTintColor: 'steelblue',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-      <Tab.Screen name="MealScreen" component={MealScreen} />
-      <Tab.Screen name="CocktailScreen" component={CocktailScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Meals') {
+              iconName = 'food';
+            } else if (route.name === 'Cocktails') {
+              iconName = 'glass-cocktail';
+            }
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'steelblue',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Meals" component={MealStack} options={{ headerShown: false }} />
+        <Tab.Screen name="Cocktails" component={CocktailStack} options={{ headerShown: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
+
 export default App;
