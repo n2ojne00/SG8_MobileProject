26d68c3127001123ea0f1f285e87736e078386d3
Elias K <127874391+n2koel00@users.noreply.github.com>
2024-11-04 20:18:14 +0200

    style main and login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,77 +1,28 @@
 import React from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import styles from "../styles/style";
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Image source={require('../kuvat/drink.jpg')} style={styles.logo} />
+    <View style={styles.containerLogin}>
+      <Image source={require('../kuvat/drink.jpg')} style={styles.logoLogin} />
       <Text style={styles.title}>Welcome to the App</Text>
       <TextInput
-        style={styles.input}
+        style={styles.inputLogin}
         placeholder="Email"
         placeholderTextColor="#888"
       />
       <TextInput
-        style={styles.input}
+        style={styles.inputLogin}
         placeholder="Password"
         placeholderTextColor="#888"
         secureTextEntry
       />
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('MainApp')}>
-        <Text style={styles.buttonText}>Enter Site</Text>
+      <TouchableOpacity style={styles.buttonLogin} onPress={() => navigation.navigate('MainApp')}>
+        <Text style={styles.buttonTextLogin}>Enter Site</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f9f9f9',
-    padding: 25,
-  },
-  logo: {
-    width: 400,
-    height: 300,
-    marginBottom: 20,
-    borderRadius: 10,
-    marginBottom: 24,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 24,
-    textAlign: 'center',
-    color: '#333',
-  },
-  input: {
-    width: '100%',
-    height: 50,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 25,
-    paddingHorizontal: 16,
-    marginBottom: 16,
-    backgroundColor: '#fff',
-    elevation: 2,
-  },
-  button: {
-    width: '100%',
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: 'steelblue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
-    elevation: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-});
 
 export default LoginScreen;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,33 +1,16 @@
 // MainScreen.js
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import styles from "../styles/style";
+import { View, Text, Button } from 'react-native';
 
 const MainScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Main Screen</Text>
+    <View style={styles.containerMain}>
+      <Text style={styles.titleMain}>Main Screen</Text>
       <Text>Welcome to the Main Screen!</Text>
      
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f9f9f9',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    marginTop: 20,
-    width: '80%',
-  },
-});
 
 export default MainScreen;
