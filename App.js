bcb9a073e792d7f8df42df0768451d9ed9376997
Jonssoni <n2nejo00@students.oamk.fi>
2024-11-04 17:52:42 +0200

    footer and header added

--- App.js
+++ App.js
@@ -5,17 +5,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import CocktailScreen from './CocktailScreen';
 import CocktailDetailScreen from './CocktailDetailScreen';
+import Footer from './screens/Footer';
+import Header from './screens/Header';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
+       <Header/>
       <Stack.Navigator initialRouteName="Login">
+       
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Cocktail" component={CocktailScreen} options={{ title: 'Cocktail Search' }} />
         <Stack.Screen name="CocktailDetail" component={CocktailDetailScreen} options={{ title: 'Cocktail Details' }} />
+        
       </Stack.Navigator>
+      <Footer/>
     </NavigationContainer>
   );
 };
