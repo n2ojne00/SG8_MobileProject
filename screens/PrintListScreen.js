d31a39827011b64642bbcf1708d38436f03932d9
Netta <n2ojne00@students.oamk.fi>
2024-12-09 18:42:56 +0200

    Printlist theme changes

--- screens/PrintListScreen.js
+++ screens/PrintListScreen.js
@@ -7,32 +7,48 @@ import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 import { MainStyles } from '../styles/MainScreenStyles';
 import { PrintList } from '../styles/PrintListStyles';
 import { MealAndDrink } from '../styles/MealsAndDrinks';
+import { useTheme } from '../contexts/ThemeContext';
 
 
 const PrintListScreen = ({ route, navigation }) => {
+  const { theme } = useTheme(); // Access the current theme from context
+
   const { list } = route.params;
 
   return (
     <ImageBackground style={globalStyles.background} resizeMode="cover">
       <ThemeLayout>
         <View style={globalStyles.container}>
-          <View style={PrintList.innerContainerPrintList}>
+          <View style={[PrintList.innerContainerPrintList, { backgroundColor: theme.bgTransparentLightGreen }]}>
             <View
-              style={[MainStyles.RecipeButton, { marginTop: 12, alignSelf: 'center' }]}>
-              <FontAwesome5 name='shopping-basket' size={25} color="#386641" />;
+              style={[MainStyles.RecipeButton, { marginTop: 12, alignSelf: 'center', backgroundColor: theme.bgRecipeBtn }]}>
+              <FontAwesome5 name='shopping-basket' size={25} color={theme.textDarkGreen} />;
             </View>
 
-            <Text style={MealAndDrink.titleDS}>{list?.name || 'Shopping List'}</Text>
+            <Text style={[MealAndDrink.titleDS, { color: theme.textDarkGreen, borderColor: theme.borderSearch }]}>{list?.name || 'Shopping List'}</Text>
             <FlatList
               data={list?.items || []}
               keyExtractor={(item, index) => index.toString()}
               renderItem={({ item }) => (
-                <Text style={PrintList.shoppinglistItem}>{item}</Text>
+                <Text style={[PrintList.shoppinglistItem,
+                {
+                  borderColor: theme.borderSearch,
+                  backgroundColor: theme.bgSaveBtn,
+                  color: theme.textDarkGreen
+                }]}>
+                  {item}</Text>
               )}
             />
-            <Pressable style={[styles.createButton, { alignSelf: 'center' }]} onPress={() => navigation.goBack()} >
-              <Ionicons name="arrow-back-outline" size={15} color="#386641" style={{ marginHorizontal: 15 }} />
-              <Text style={styles.buttonTextRL}>Back to shopping list</Text>
+            <Pressable style={[PrintList.createButton,
+            {
+              alignSelf: 'center',
+              borderColor: theme.borderSearch,
+              backgroundColor: theme.bgSaveBtn,
+              color: theme.textDarkGreen
+            }]}
+              onPress={() => navigation.goBack()} >
+              <Ionicons name="arrow-back-outline" size={15} color={theme.textDarkGreen} style={{ marginHorizontal: 15 }} />
+              <Text style={[PrintList.buttonTextRL, {color: theme.textDarkGreen}]}>Back to shopping list</Text>
             </Pressable>
           </View>
         </View>
--- styles/PrintListStyles.js
+++ styles/PrintListStyles.js
@@ -8,18 +8,28 @@ export const PrintList = StyleSheet.create({
     paddingVertical: 8, 
     paddingHorizontal: 15, 
     borderWidth: 1, 
-    borderColor: '#6A994E', 
-    backgroundColor: '#dbfaeb',
     borderRadius: 8, 
     textAlign: 'center',
-    color: '#386641', 
     width: '90%',
     alignSelf: 'center',
   },
   innerContainerPrintList: {
     borderRadius: 10,
-    backgroundColor: '#f3fff5a9',
     height: '100%',
+  },
+  createButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 15,
+    width: 200,
+    height: 40,
+    borderRadius: 30,
+    borderWidth: 2,
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  buttonTextRL: {
+    fontSize: 16,
   },
 
 })
